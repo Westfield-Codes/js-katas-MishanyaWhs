9622c6c02a3f968abe180e7d5a15eec0e92d9f22
MishanyaWhs <Mykhailo208202@student-schoolsofwestfield.org>
2024-09-17 12:07:47 -0400

    color Kata

--- done.js
+++ done.js
@@ -4,6 +4,15 @@
 
 /* No Prompt */
 
+var color = "Red"; 
+// alert the message" "My favorite color is " plus the color
+alert("My favorite color is " + color);
+
+/* Var Alert Prompt */
+// make a string variable for color, prompt the user for their favorite
+color = prompt(What is your favorite color?);
+// alert color " is a nice color!"
+alert(color + " is a nice color);
 
 /* Prompt */
 
